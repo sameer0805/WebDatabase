5c3c34f08b63e197a717d51180b1675a73753ed3
Sameer Pandhare <sammyp@Sameers-MacBook-Pro.local>
2017-11-09 17:01:44 -0500

    Added an attempt at POST route for new patient

--- basicweb.py
+++ basicweb.py
@@ -1,10 +1,30 @@
+from flask import Flask, request, jsonify
+app = Flask(__name__)
 from pymodm import connect
 from pymodm import MongoModel, fields
+
 connect("mongodb://localhost:27017/bme590")
+
+# class User(MongoModel): ## template for an entry
+#    email = fields.EmailField(primary_key=True)
+#    first_name = fields.CharField()
+#    last_name = fields.CharField()
+# u = User('user1@email.com', last_name='Ross', first_name='Bob')
+# u.save()
+
 class User(MongoModel):
-    email = fields.EmailField(primary_key=True)
-    first_name = fields.CharField()
-    last_name = fields.CharField()
-u = User('user1@email.com', last_name='Ross', first_name='Bob')
-u.save()
+    Name = fields.CharField()
+    Age = fields.CharField()
+    BMI = fields.CharField()
+
+@app.route("/new_patient", methods = ['POST'])
+def newpatient():
+    data = request.get_json()
+    Name = data['Name']
+    Age = data['Age']
+    BMI = data['BMI']
+    u = User(Name, Age, BMI)
+    u.save()
+    return jsonify(u)
+
 
