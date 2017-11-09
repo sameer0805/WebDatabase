e2c9da75ef614186d0527d794a831b8515fabbf4
Sameer Pandhare <sammyp@Sameers-MacBook-Pro.local>
2017-11-09 16:11:41 -0500

    Testing PyCharm commit

--- basicweb.py
+++ basicweb.py
@@ -1,13 +1,10 @@
 from pymodm import connect
 from pymodm import MongoModel, fields
-
 connect("mongodb://localhost:27017/bme590")
-
 class User(MongoModel):
     email = fields.EmailField(primary_key=True)
     first_name = fields.CharField()
     last_name = fields.CharField()
-
 u = User('user1@email.com', last_name='Ross', first_name='Bob')
 u.save()
 
