2ba693a183c9ac708c476639cbbc3dce2d4c2069
Sameer Pandhare <sammyp@Sameers-MacBook-Pro.local>
2017-12-05 16:58:50 -0500

    Still trying to resolve merge conflicts

--- react-learning/melanomapredictions.py
+++ react-learning/melanomapredictions.py
@@ -1,15 +1,12 @@
-from get_prediction import get_prediction
+from bme590_melanoma_prediction.get_prediction import get_prediction
 import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
 
-img = mpimg.imread(# this needs to be a string, URL, python object)
-
-
-
+img = mpimg.imread('/Users/sammyp/Desktop/Melanoma.jpg')
 print("img type:", type(img))
 print("img shape:", img.shape)
-
 (labels, predictions) = get_prediction(img)
 print("\n\nPredictions:")
 print(labels)
 print (predictions)
+
--- react-learning/meldiameter.py
+++ react-learning/meldiameter.py
@@ -1,5 +1,6 @@
 import numpy as np
 import cv2
+import base64
 
 im = cv2.imread('/Users/sammyp/Desktop/Melanoma.jpg')
 imgray = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
@@ -9,10 +10,8 @@ edged = cv2.Canny(FilteredImage,100,200)
 image, contours, hierarchy = cv2.findContours(edged,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 img = cv2.drawContours(im, contours, -1, (0,255,0), 3)
 
-
-cv2.imshow('Image with Filtering and Edge Detection', img)
+cv2.imshow('Image with Filtering and Edge Detection', edged)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-
 area = cv2.contourArea(contours)
 diameter = np.sqrt(4*area/np.pi)
