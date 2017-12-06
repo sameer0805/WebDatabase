63ca8c1a1bf290477fd59539ec3ae0581a7e1906
Sameer Pandhare <sammyp@Sameers-MacBook-Pro.local>
2017-12-05 22:35:13 -0500

    More front end changes

--- react-learning/teaching2/src/App.js
+++ react-learning/teaching2/src/App.js
@@ -3,10 +3,19 @@ import logo from './logo.svg';
 import './App.css';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import AppBar from 'material-ui/AppBar';
+// import Drawer from 'material-ui/Drawer';
+// import MenuItem from 'material-ui/MenuItem';
 import Upload from './upload';
 import PatientForm from './textbox';
 
 class App extends Component {
+  constructor(props) {
+        super(props)
+        this.state = {open: false};
+        }
+
+  handleToggle = () => this.setState({open: !this.state.open});
+
   render() {
     return (
       <div>
@@ -21,3 +30,11 @@ class App extends Component {
 }
 
 export default App;
+
+
+
+// onLeftIconButtonClick{this.handleToggle}/>
+            // <Drawer open = {this.state.open}>
+            // <MenuItem> Menu Item </MenuItem>
+            // <MenuItem> Menu Item 2 </MenuItem>
+            // </Drawer>
