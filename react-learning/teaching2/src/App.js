8a3ac21d009f0dafd421233448a64be623a74a86
Sameer Pandhare <sammyp@Sameers-MacBook-Pro.local>
2017-12-06 22:53:01 -0500

    Front end, but menu item is still not working and need to figure out how to center components

--- react-learning/teaching2/src/App.js
+++ react-learning/teaching2/src/App.js
@@ -1,26 +1,50 @@
 import React, { Component } from 'react';
+// import View from 'react-native';
 import logo from './logo.svg';
 import './App.css';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import AppBar from 'material-ui/AppBar';
-// import Drawer from 'material-ui/Drawer';
-// import MenuItem from 'material-ui/MenuItem';
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
 import Upload from './upload';
 import PatientForm from './textbox';
+// import styles from './centering'
 
 class App extends Component {
-  constructor(props) {
+        constructor(props) {
         super(props)
         this.state = {open: false};
+        this.handleToggle = this.handleToggle.bind(this)
+        this.handleClose = this.handleClose.bind(this)
         }
 
-  handleToggle = () => this.setState({open: !this.state.open});
+  handleToggle = ()  => {
+    this.setState({open: !this.state.open});
+  }
+
+  handleClose() {
+      this.setState({open: true});
+  }
 
   render() {
     return (
       <div>
       <MuiThemeProvider>
-            <AppBar style = {{'width': '100%'}} title = "Melanoma Detector" showMenuIconButton = {true}/>
+            <AppBar style = {{'width': '100%'}} title = "Melanoma Detector" align = 'center'
+            showMenuIconButton = {true}
+            onMenuIconButtonClick = {this.handleToggle.bind(this)}
+            />
+            <Drawer
+            docked={true}
+            open={this.state.open}
+            onRequestChange={(open) => this.setState({open})}
+            >
+            <MenuItem> Melanoma Predictions </MenuItem>
+            <MenuItem> Melanoma Asymmetry</MenuItem>
+            <MenuItem> Melanoma Color </MenuItem>
+            <MenuItem> Melanoma Border </MenuItem>
+            <MenuItem> Melanoma Diameter </MenuItem>
+            </Drawer>
             <Upload/>
             <PatientForm/>
       </MuiThemeProvider>
@@ -30,11 +54,3 @@ class App extends Component {
 }
 
 export default App;
-
-
-
-// onLeftIconButtonClick{this.handleToggle}/>
-            // <Drawer open = {this.state.open}>
-            // <MenuItem> Menu Item </MenuItem>
-            // <MenuItem> Menu Item 2 </MenuItem>
-            // </Drawer>
---
+++ react-learning/teaching2/src/centering.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import View from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+  }
+});
+
+export default styles;
