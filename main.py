d7a231282a8057b711fc97040a9138303b72291d
Alice <alice@Alices-MacBook-Pro.local>
2017-12-13 14:49:35 -0500

    fixed pep8 conventions

--- main.py
+++ main.py
@@ -1,51 +1,52 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from time import gmtime, strftime
+from meldiameter import contour
+from melcolor import colorplot
 import base64
 import re
 import numpy as np
 from setup_db import User, write_file
 from melanomapredictions import predict
 import sys
-sys.path.insert(0,'/notebooks/react-learning/')
-sys.path.insert(0,'/notebooks/react-learning/melcolor.py')
-from meldiameter import contour
-from melcolor import colorplot
+sys.path.insert(0, '/notebooks/react-learning/')
+sys.path.insert(0, '/notebooks/react-learning/melcolor.py')
 
 app = Flask(__name__)
 CORS(app)
+
 
 def determine_filepath(img_string):
     filename = strftime("%Y%m%d_%H_%M_%S", gmtime())
     PATH = "images/"
     check_jpg = re.search("^data:image/jpeg", img_string)
-    if (check_jpg != None):
-	    file_ending = ".jpg"
+    if (check_jpg is not None):
+        file_ending = ".jpg"
     else:
         check_png = re.search("^data:image/png", img_string)
-        if (check_png != None):
+        if (check_png is not None):
             file_ending = ".png"
         else:
             check_gif = re.search("^data:image/bmp", img_string)
-            if (check_gif != None):
+            if (check_gif is not None):
                 file_ending = ".bmp"
             else:
                 file_ending = ""
-             #   error_string = "Error: Incorrect file type uploaded"
-              #  return error_string
     filepath = PATH + filename + file_ending
     filepath_without_end = PATH + filename
     return filepath, filepath_without_end, file_ending
 
+
 def analyze_image(filepath, filepath_without_end, file_ending, image_string):
     if file_ending == "":
         labels2 = "Error: Incorrect file type uploaded"
-        predict5 = ["Error","Incorrect file type"]
+        predict5 = ["Error", "Incorrect file type"]
         contour_img_post = ""
         colorplot_img_post = ""
     else:
-        with open (filepath, "w") as image_out:
-            image_out.write(re.sub('^data:image/.+;base64,', '', image_string).decode('base64'))
+        with open(filepath, "w") as image_out:
+            image_out.write(re.sub('^data:image/.+;base64,', '',
+                                   mage_string).decode('base64'))
         labels, predictions = predict(filepath)
         name = "test"
         predict2 = predictions.tolist()
@@ -55,22 +56,27 @@ def analyze_image(filepath, filepath_without_end, file_ending, image_string):
         predict5 = predict4.tolist()
         colorplot_path = filepath_without_end + "_colorhistorgram.jpg"
         colorplot_img_string = colorplot(filepath, "histogram", ".jpg")
-        contour_img_string = contour(filepath, 175, 5, "contour",".jpg")
+        contour_img_string = contour(filepath, 175, 5, "contour", ".jpg")
         colorplot_img_post = "data:image/jpeg;base64," + colorplot_img_string
         contour_img_post = "data:image/jpeg;base64," + contour_img_string
-    results = {"labels": labels2, "probabilities": predict5, "contour": contour_img_post,
-               "colorplot": colorplot_img_post}
+    results = {"labels": labels2, "probabilities": predict5,
+               "contour": contour_img_post, "colorplot": colorplot_img_post}
     return results
+
 
 @app.route('/test')
 def hello_world():
     return 'Hello, world2'
 
+
 @app.route('/', methods=['POST'])
 def validate():
     img = request.get_json()
     image_string = img['image']
-    filepath, filepath_without_end, file_ending = determine_filepath(image_string)
-    results = analyze_image(filepath, filepath_without_end, file_ending, image_string)
+    filepath, filepath_without_end, file_ending = \
+        determine_filepath(image_string)
+    results = analyze_image(filepath, filepath_without_end,
+                            file_ending, image_string)
     return jsonify(results)
-	#write_file(name,file_ending,filepath,malignant,benign,symmetry,border,color,diameter)
+    # write_file(name,file_ending,filepath,malignant,benign,
+    # symmetry,border,color,diameter)
--- react-learning/melcolor.py
+++ react-learning/melcolor.py
@@ -1,4 +1,5 @@
-def colorplot(imagepath = '/Users/Ben/Desktop/melanoma.jpg', save_file_title = 'histogram',save_file_type = '.jpg'):
+def colorplot(imagepath='/Users/Ben/Desktop/melanoma.jpg',
+              save_file_title='histogram', save_file_type='.jpg'):
     import numpy as np
     import base64
     import cv2
@@ -6,40 +7,39 @@ def colorplot(imagepath = '/Users/Ben/Desktop/melanoma.jpg', save_file_title = '
     matplotlib.use('Agg')
     import matplotlib.pyplot as plt
     im = cv2.imread(imagepath)
-    colorimage = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
+    colorimage = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
     # hist = cv2.calcHist([colorimage], [0], None, [256], [0, 256])
     # plt.plot(hist)
 
-    ## need to reshape the image to be a list of pixels
+    # need to reshape the image to be a list of pixels
     colorimage = colorimage.reshape((colorimage.size, 1))
     imdata = np.float32(colorimage)
 
-    ## setting the criteria for the algorithm
+    # setting the criteria for the algorithm
     criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
 
-    ## how are initial centers taken
+    # how are initial centers taken
     flags = cv2.KMEANS_RANDOM_CENTERS
 
-    compactness, labels, centers = cv2.kmeans(imdata,2,criteria,10,flags)
-    A = imdata[labels==0]
-    B = imdata[labels==1]
+    compactness, labels, centers = cv2.kmeans(imdata, 2, criteria, 10, flags)
+    A = imdata[labels == 0]
+    B = imdata[labels == 1]
     fig = plt.figure(1)
     fig.add_subplot(111)
-    plt.hist(A,256,[0,256],color = 'r')
-    plt.hist(B,256,[0,256],color = 'b')
-    plt.hist(centers,32,[0,256],color = 'y')
+    plt.hist(A, 256, [0, 256], color='r')
+    plt.hist(B, 256, [0, 256], color='b')
+    plt.hist(centers, 32, [0, 256], color='y')
     plt.xlabel('Pixel Value')
     plt.ylabel('Intensity')
     plt.title('Color Histogram')
-    #save_file_title = 'histogram'
-    #save_file_type = '.jpg'
+    # save_file_title = 'histogram'
+    # save_file_type = '.jpg'
     save_file_name = save_file_title + save_file_type
     plt.savefig(save_file_name)
     plt.show()
 
-    plot_string = base64.b64encode(open(save_file_name,'rb').read())
+    plot_string = base64.b64encode(open(save_file_name, 'rb').read())
     return plot_string
-    #decoded_string = base64.b64decode(plot_string)
-    #result = open('decodedimage.jpg','wb')
-    #result.write(decoded_string)
-
+    # decoded_string = base64.b64decode(plot_string)
+    # result = open('decodedimage.jpg','wb')
+    # result.write(decoded_string)
